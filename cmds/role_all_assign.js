e1401435c62478e6020a7227c01749d9ab5e271a
Matt Strenk <mstrenk12@gmail.com>
2019-12-14 17:33:15 -0500

    Update role_all_assign.js

--- cmds/role_all_assign.js
+++ cmds/role_all_assign.js
@@ -1,12 +1,19 @@
-module.exports.run = (client, message, args) => {
-    let role = message.guild.roles.find(r => r.name == 'Fuck Cancer')
+module.exports.run = async (client, message, args) => {
+    let role = message.guild.roles.find(r => r.name == 'Fuck Cancer');
 
-    if (!role) return message.channel.send(`**${message.author.username}**, role not found`)
+    if (!role) 
+        return message.channel.send(`**${message.author.username}**, role not found`);
 
-    message.guild.members.filter(m => !m.user.bot).forEach(member => member.addRole(role))
-    message.channel.send(`**${message.author.username}**, role **${role.name}** was added to all members`)
+    message.guild.members.filter(m => !m.user.bot).forEach(member => {
+        massRole(member, role);
+    });
+    message.channel.send(`**${message.author.username}**, role **${role.name}** was added to all members`);
 }
 
 module.exports.help = {
     name: "roleassign"
+}
+
+async function massRole(user, role) {
+    user.addRole(role);
 }

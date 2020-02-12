ce07f61eb0c63969c5cc1b8ffa9de991ea573753
Matt Strenk <mstrenk12@gmail.com>
2020-02-11 22:19:47 -0500

    Start adding game stats

--- cmds/gameStats.js
+++ cmds/gameStats.js
@@ -1,43 +1,73 @@
 const request = require("request");
 const Discord = require("discord.js");
-const config = require("../config.json");
-const game = require("../json_classes/game.js");
+const teamInfo = require("../helpers/teamInfo.js");
 
 module.exports.run = (client, message, args) => {
-  url = "https://statsapi.web.nhl.com/api/v1/game/2018020001/feed/live";
+  url = "https://statsapi.web.nhl.com/api/v1/game/2018020004/boxscore";
 
-  request({ url: url, json: true }, function (error, response, body) {
+  request({ url: url, json: true }, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      var gameObj = game.gameToJson(body);
+      let bodyStr = JSON.stringify(body);
+      var obj = JSON.parse(bodyStr);
+
+      var homeTeamName = obj.teams.home.team.name;
+      var awayTeamName = obj.teams.away.team.name;
+
+      var homeStats = obj.teams.home.teamStats.teamSkaterStats;
+      var awayStats = obj.teams.away.teamStats.teamSkaterStats;
+
+      var homeEmoji = client.emojis.find(
+        emoji => emoji.name === teamInfo.getEmoji(homeTeamName)
+      );
+      var awayEmoji = client.emojis.find(
+        emoji => emoji.name === teamInfo.getEmoji(awayTeamName)
+      );
+
+      // Check who won
+      if (homeStats.goals > awayStats.goals) {
+        var winner = homeTeamName;
+      } else {
+        var winner = awayTeamName;
+      }
 
       message.channel.send({
         embed: {
-          color: 16711680,
-          title: "NHL Bot Help",
+          color: teamInfo.getHomeColor(winner),
+          title: `${awayEmoji} ${awayTeamName} @ ${homeEmoji} ${homeTeamName}`,
+          thumbnail: {
+            url: teamInfo.getLogo(winner)
+          },
           fields: [
             {
-              name: prefix + "schedule [number of games]",
-              value: "Prints up to the next specified number of Flyers games.  The default is 5."
+              name: awayTeamName,
+              value: getStats(awayStats),
+              inline: true
             },
             {
-              name: prefix + "standings help",
-              value: "Prints the help information for the standings command."
-            },
-            {
-              name: prefix + "career [player's full name]",
-              value: "Prints a players career stats."
-            },
-            {
-              name: prefix + "season [player's full name]",
-              value: "Prints a players stats for the current season."
+              name: homeTeamName,
+              value: getStats(homeStats),
+              inline: true
             }
           ]
         }
       });
     }
   });
+};
+
+function getStats(stats) {
+  let formattedStats = `**Goals:** ${stats.goals}
+  **Shots:** ${stats.shots}
+
+  **Shots Blocked:** ${stats.blocked}
+  **Give Aways:** ${stats.giveaways}
+  **PP%:** ${stats.powerPlayPercentage}
+  **FOW%:** ${stats.faceOffWinPercentage}
+  **PIM:** ${stats.pim}
+  **Hits:** ${stats.hits}`;
+  return formattedStats;
 }
 
 module.exports.help = {
-  name: "gamestats"
-}
+  name: "gs"
+};

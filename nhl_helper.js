aae29f6b4a70f1999e2d2a367a9040da24aaa2ff
Matt Strenk <mstrenk12@gmail.com>
2020-05-20 01:48:50 -0400

    Added send message for level up number

--- nhl_helper.js
+++ nhl_helper.js
@@ -76,17 +76,28 @@ client.on("message", (message) => {
     let regex = /level ([0-9]+) !/;
     let res = message.content.match(regex);
     let pNum = res[1];
-    exec(`curl -s 'http://www.flyershistory.com/cgi-bin/rosternum.cgi?${pNum}' | hxnormalize -l 1024 -x | hxselect -c -s '\n' 'tbody tr td a font'`, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+    exec(
+      `curl -s 'http://www.flyershistory.com/cgi-bin/rosternum.cgi?${pNum}' | hxnormalize -l 1024 -x | hxselect -c -s '\n' 'tbody tr td a font'`,
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          return;
+        }
+        if (stdout.length != 0) {
+          message.channel.send(
+            `The following Flyers players have had the number ${pNum}:\n${stdout}`
+          );
+        } else {
+          message.channel.send(
+            `No Flyers player has ever had the number ${pNum}!`
+          );
+        }
       }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-    });
+    );
   }
 
   if (message.author.bot) return; // ignores all bots

d85972544fd4069801d35b07563958e6a86c8236
Matt Strenk <strenkml@users.noreply.github.com>
2020-12-23 05:19:16 -0500

    Start adding logic for live game updates

--- nhl_helper.js
+++ nhl_helper.js
@@ -4,22 +4,6 @@ const config = require("./config.json");
 const fs = require("fs");
 const { exec } = require("child_process");
 
-const express = require("express");
-const app = express();
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/webpage/index.html");
-});
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
-
 var prefix = null;
 if (config.testMode) {
   prefix = "!";
@@ -119,4 +103,99 @@ if (config.testMode) {
   client.login(config.testToken);
 } else {
   client.login(config.token);
+}
+
+var lastUpdateTime = 0;
+
+// Check for live game data every 30 seconds
+setInterval(checkGameData, 30);
+
+function checkGameData() {
+  var currentGame = getCurrentGame();
+  if (currentGame != 0) {
+    var url = `https://statsapi.web.nhl.com/api/v1/game/${currentGame}/feed/live/diffPatch?startTimecode=${lastUpdateTime}`;
+    request({ url: url, json: true }, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        let string = JSON.stringify(body);
+        var obj = JSON.parse(string);
+
+        var allPlays = obj.liveData.plays.allPlays;
+
+        // Loop through all of the events since the last check
+        allPlays.forEach((play) => {
+          let eventType = play.result.eventTypeId;
+          if (eventType == "PERIOD_START") {
+            sendPeriodStartMessage(play);
+          } else if (eventType == "PERIOD_END") {
+            sendPeriodEndMessage(play);
+          } else if (eventType == "GAME_END") {
+            sendGameEndMessage(play);
+          } else if (eventType == "GOAL") {
+            sendGoalMessage(play);
+          }
+        });
+      }
+    });
+  }
+  lastUpdateTime = getDateTime();
+}
+
+// If there is a game for the current day, get the game id
+function getCurrentGame() {
+  var date = getDate();
+  var url = `https://statsapi.web.nhl.com/api/v1/schedule?teamId=4&date=${date}`;
+  request({ url: url, json: true }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      let string = JSON.stringify(body);
+      var obj = JSON.parse(string);
+      if (obj.dates.length == 1) {
+        return obj.dates[0].games[0].gamePk;
+      } else {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
+  });
+}
+
+// Gets the current date in the format: YYYY-MM-DD
+function getDate() {
+  let ts = Date.now();
+  let date = new Date(ts);
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+
+  return `${year}-${month}-${day}`;
+}
+
+// Gets the current date and time in the format: YYYYMMDD_hhmmss
+function getDateTime() {
+  let ts = Date.now();
+  let date = new Date(ts);
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  return `${year}${month}${day}_${hours}${minutes}${seconds}`;
+}
+
+function sendGoalMessage(play) {
+  var embed = new Discord.RichEmbed();
+}
+
+function sendPeriodStartMessage(play) {
+  var embed = new Discord.RichEmbed();
+}
+
+function sendPeriodEndMessage(play) {
+  var embed = new Discord.RichEmbed();
+}
+
+function sendGameEndMessage(play) {
+  var embed = new Discord.RichEmbed();
 }

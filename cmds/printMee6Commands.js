d1bf04372a8178a481e07022c9b9e23bf5366e32
Matt Strenk <strenkml@users.noreply.github.com>
2021-07-30 22:56:39 -0400

    Removed mee6 command tester.  Changed functionality to list the commands

--- cmds/printMee6Commands.js
+++ cmds/printMee6Commands.js
@@ -1,9 +1,7 @@
-const globals = require("../helpers/globals");
-
 module.exports.run = async (client, message, args) => {
   if (
     message.channel.id == 345701810616532993 ||
-    message.channel.id == 495745156163698688
+    message.channel.id == 743100445282795621
   ) {
     message.delete();
     if (args.length == 0) return;
@@ -13,18 +11,18 @@ module.exports.run = async (client, message, args) => {
     let emptyLineRegex = /^\n/gm;
     all = all.replace(emptyLineRegex, "");
 
-    var lines = all.split("\n");
-    var index = 0;
-    globals.timer = setInterval(() => {
-      if (index == lines.length) {
-        clearInterval(globals.timer);
-      }
-      message.channel.send(lines[index]);
-      index++;
-    }, 5000);
+    var lines = all.split("\n").length;
+
+    var now = new Date();
+    var mm = now.getMonth() + 1;
+    var dd = now.getDate();
+    var yy = now.getFullYear().toString().slice(-2);
+
+    var header = `**Mee6 Commands as of ${mm}/${dd}/${yy} (${lines} commands)**`;
+    message.channel.send(`${header}\n${all}`);
   }
 };
 
 module.exports.help = {
-  name: "mee6Test",
+  name: "mee6Cmds",
 };

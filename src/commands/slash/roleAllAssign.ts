0405a4f2853560102178deb8b95f16d6d4010a0b
Matt Strenk <strenkml@users.noreply.github.com>
2023-02-18 03:35:42 -0500

    Change the get param value method. Create roleAllAssign command

--- src/commands/slash/roleAllAssign.ts
+++ src/commands/slash/roleAllAssign.ts
@@ -1,6 +1,6 @@
-import { CommandInteraction } from "discord.js";
+import { ChatInputCommandInteraction, Role } from "discord.js";
 
-import { AdminSlashCommand } from "../../models/SlashCommand";
+import { AdminSlashCommand, PARAM_TYPES } from "../../models/SlashCommand";
 
 export default class RoleAllAssignCommand extends AdminSlashCommand {
   constructor() {
@@ -10,10 +10,41 @@ export default class RoleAllAssignCommand extends AdminSlashCommand {
       .setDMPermission(false)
       .addRoleOption((option) =>
         option.setName("role").setDescription("The role to assign to everyone").setRequired(true)
+      )
+      .addBooleanOption((option) =>
+        option
+          .setName("onlyNoRole")
+          .setDescription("Whether or not to give the role to only members that dont have a role")
       );
   }
 
-  async execute(interaction: CommandInteraction): Promise<void> {
-    interaction.reply({ content: "Pong", ephemeral: true });
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    const role: Role = this.getParamValue(interaction, PARAM_TYPES.ROLE, "role");
+    const onlyNoRole: boolean = this.getParamValue(interaction, PARAM_TYPES.BOOLEAN, "onlyNoRole") || true;
+    if (role) {
+      const members = await interaction.guild?.members.fetch();
+      if (!members) {
+        interaction.reply({ content: "Error retreiving the guild members", ephemeral: true });
+        return;
+      }
+
+      members.forEach((member) => {
+        if (onlyNoRole && member.roles.cache.size == 0) {
+          member.roles.add(role);
+        } else if (!onlyNoRole) {
+          member.roles.add(role);
+        }
+      });
+
+      if (onlyNoRole) {
+        interaction.reply({
+          content: `Adding the following role to users that do not have a role: ${role.name}`,
+          ephemeral: true,
+        });
+      } else {
+        interaction.reply({ content: `Adding the following role to all users: ${role.name}`, ephemeral: true });
+      }
+    }
+    interaction.reply({ content: "Error finding the role!", ephemeral: true });
   }
 }
--- src/models/SlashCommand.ts
+++ src/models/SlashCommand.ts
@@ -14,10 +14,35 @@ export abstract class SlashCommand {
     this.data = new SlashCommandBuilder().setName(this.name).setDescription(this.description);
   }
 
-  protected getParameterValue(parameterName: string, interaction: ChatInputCommandInteraction): string {
-    const val = interaction.options.get(parameterName)?.value;
-
-    return val == undefined ? "" : val.toString();
+  protected getParamValue(interaction: ChatInputCommandInteraction, type: PARAM_TYPES, paramName: string): any | null {
+    let val = undefined;
+    switch (type) {
+      case PARAM_TYPES.STRING:
+        val = interaction.options.getString(paramName) || "";
+        break;
+      case PARAM_TYPES.ROLE:
+        val = interaction.options.getRole(paramName);
+        break;
+      case PARAM_TYPES.BOOLEAN:
+        val = interaction.options.getBoolean(paramName);
+        break;
+      case PARAM_TYPES.CHANNEL:
+        val = interaction.options.getChannel(paramName);
+        break;
+      case PARAM_TYPES.ATTACHMENT:
+        val = interaction.options.getAttachment(paramName);
+        break;
+      case PARAM_TYPES.INTEGER:
+        val = interaction.options.getInteger(paramName);
+        break;
+      case PARAM_TYPES.MEMBER:
+        val = interaction.options.getMember(paramName);
+        break;
+      case PARAM_TYPES.USER:
+        val = interaction.options.getUser(paramName);
+        break;
+    }
+    return val;
   }
 
   abstract execute(interaction: ChatInputCommandInteraction): Promise<void>;
@@ -29,4 +54,15 @@ export abstract class AdminSlashCommand extends SlashCommand {
 
     this.data.setDefaultMemberPermissions(PermissionsBitField.Flags.Administrator);
   }
+}
+
+export enum PARAM_TYPES {
+  STRING,
+  INTEGER,
+  BOOLEAN,
+  ROLE,
+  CHANNEL,
+  USER,
+  MEMBER,
+  ATTACHMENT,
 }

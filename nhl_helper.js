eaa41f5cc2a16513057a664063bfa2cb67296cbc
Matt Strenk <mstrenk12@gmail.com>
2024-02-18 03:14:12 +0000

    Temp disable

--- nhl_helper.js
+++ nhl_helper.js
@@ -147,11 +147,15 @@ globals.client.on("message", (message) => {
   if (message.author.bot) return; // ignores all bots
   if (message.channel.type != "text") return; // ignores all dm's
 
+
+  // Check for Carter Hart
+
+
   if (message.content.toLowerCase().includes("carter") || message.content.toLowerCase().includes("hart") ) {
     // Message contains carter or hart
     if (!message.content.toLowerCase().includes("jeff")) {
-      message.channel.send("Fuck Carter Hart!")
-      return;
+      // message.channel.send("Fuck Carter Hart!")
+      // return;
     }
   }
 

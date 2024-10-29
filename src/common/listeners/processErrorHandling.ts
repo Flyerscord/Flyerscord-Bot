3f7820af9441e1d11805dc95b01bec66c2e407f8
default <mstrenk12@gmail.com>
2024-10-29 19:44:51 +0000

    ignore buffer.file warning

--- src/common/listeners/processErrorHandling.ts
+++ src/common/listeners/processErrorHandling.ts
@@ -8,7 +8,12 @@ export default (): void => {
   });
 
   // Process Warning
+  process.removeAllListeners("warning");
   process.on("warning", (warning) => {
+    // Ignore warning about buffer.File
+    if (warning.name === "ExperimentalWarning" && warning.message.includes("buffer.File")) {
+      return;
+    }
     Stumper.warning(warning.message, warning.name);
     Stumper.warning(warning.stack, warning.name);
   });

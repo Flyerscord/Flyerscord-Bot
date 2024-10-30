1a04ddb3e5efcb7ce6bd3320c19e43379adb8869
default <mstrenk12@gmail.com>
2024-10-30 04:55:47 +0000

    Change ping command to include the health of the bot

--- src/modules/misc/commands/slash/PingCommand.ts
+++ src/modules/misc/commands/slash/PingCommand.ts
@@ -1,12 +1,17 @@
 import { ChatInputCommandInteraction } from "discord.js";
 import SlashCommand from "../../../../common/models/SlashCommand";
+import BotHealthManager from "../../../../common/managers/BotHealthManager";
 
 export default class PingCommand extends SlashCommand {
   constructor() {
-    super("ping", "Check if the bot is online");
+    super("ping", "Check if the bot is online and its health");
   }
 
   async execute(interaction: ChatInputCommandInteraction): Promise<void> {
-    interaction.reply({ content: "PONG!", ephemeral: true });
+    const healthManager = BotHealthManager.getInstance();
+    interaction.reply({
+      content: `PONG! ${healthManager.isHealthy() && interaction.client.isReady() ? "Bot is healthy!" : "Bot is not healthy!"}`,
+      ephemeral: true,
+    });
   }
 }

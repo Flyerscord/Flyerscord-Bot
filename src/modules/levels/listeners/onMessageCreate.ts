dcfe7b21ff1f067e4aa3e9f2e66c0bde103df025
default <mstrenk12@gmail.com>
2024-10-30 06:01:09 +0000

    Messages in DMs to the bot do not count for levels

--- src/modules/levels/listeners/onMessageCreate.ts
+++ src/modules/levels/listeners/onMessageCreate.ts
@@ -7,6 +7,7 @@ export default (): void => {
   ClientManager.getInstance().client.on("messageCreate", async (message: Message) => {
     if (message.author.bot) return;
     if (!message.channel.isTextBased()) return;
+    if (message.channel.isDMBased()) return;
 
     addMessage(message);
   });

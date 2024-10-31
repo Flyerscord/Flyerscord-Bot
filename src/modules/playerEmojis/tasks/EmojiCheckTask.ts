1ae707e07d19ce131120f83fa00a6694229f27a3
default <mstrenk12@gmail.com>
2024-10-31 16:46:31 -0400

    Change start time to 12:35am

--- src/modules/playerEmojis/tasks/EmojiCheckTask.ts
+++ src/modules/playerEmojis/tasks/EmojiCheckTask.ts
@@ -3,8 +3,8 @@ import { checkForNewEmojis } from "../utils/PlayerEmojis";
 
 export default class EmojiCheckTask extends Task {
   constructor() {
-    // Run every 15th day of the month at midnight
-    super("EmojiCheckTask", "0 0 0 15 * *");
+    // Run every 15th day of the month at 12:35 AM
+    super("EmojiCheckTask", "0 35 0 15 * *");
   }
 
   protected async execute(): Promise<void> {

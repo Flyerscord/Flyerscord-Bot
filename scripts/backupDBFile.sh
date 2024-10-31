bb5010000a2fcc3c840e588205ae87466aa49080
default <mstrenk12@gmail.com>
2024-10-31 16:44:31 -0400

    Add a shell script for backing up db file

---
+++ scripts/backupDBFile.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+containerName=$1
+backupLocation=$2
+
+if [ -z "$containerName" ]; then
+  echo "Container name is required"
+  exit 1
+fi
+
+if [ -z "$backupLocation" ]; then
+  echo "Backup location is required"
+  exit 1
+fi
+
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+newFileName="enmap_$timestamp.sqlite"
+backupPath="$backupLocation/$newFileName"
+
+echo "Backing up database file from container $containerName to $backupPath"
+
+echo "Stopping container $containerName"
+docker stop $containerName
+
+docker cp $containerName:/usr/src/app/data/enmap.sqlite $backupPath
+
+echo "Starting container $containerName"
+docker start $containerName
+
+echo "Backup complete!"

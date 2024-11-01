70a7fc1ef996020d6260296b8306679a68310ec2
default <mstrenk12@gmail.com>
2024-11-01 11:14:59 -0400

    Fix static profile pictures with canvas

--- src/modules/joinLeave/listeners/onGuildMemberAdd.ts
+++ src/modules/joinLeave/listeners/onGuildMemberAdd.ts
@@ -12,7 +12,13 @@ export default (): void => {
 
     const message = `<@${member.id}>\nWelcome to the ${bold("Go Flyers")}!! Rule #1: Fuck the Pens!`;
     const joinImageGenerator = new JoinImageGenerator(username, member.displayAvatarURL(), await discord.members.getMemberJoinPosition(member));
-    const joinPhoto = await joinImageGenerator.getImage();
+    let joinPhoto: Buffer;
+    try {
+      joinPhoto = await joinImageGenerator.getImage();
+    } catch (error) {
+      Stumper.caughtError(error, "joinLeave:onGuildMemberAdd");
+      return;
+    }
 
     await discord.messages.sendMessageAndImageBufferToChannel(Config.getConfig().joinLeaveMessageChannelId, message, joinPhoto);
     Stumper.info(`User ${username} has joined the server!`, "joinLeave:onGuildMemberAdd");

8700a1b4a9b39b24c5fbed92c87a90d8cc26dc0b
default <mstrenk12@gmail.com>
2025-03-12 23:46:47 -0400

    Generate default config

--- src/common/config/defaults.config.ts
+++ src/common/config/defaults.config.ts
@@ -1,1 +1,68 @@
-export default {};
+export default {
+  common: {
+    productionMode: false,
+    token: "",
+    logLevel: 3,
+    masterGuildId: "",
+    adminPrefix: ".",
+  },
+  admin: {},
+  bluesky: {
+    username: "",
+    password: "",
+    channelId: "",
+    listId: "",
+  },
+  customcommands: {
+    prefix: "!",
+    commandTempChannelId: "",
+    customCommandListChannelId: "",
+    imageKit: {
+      publicKey: "",
+      privateKey: "",
+      urlEndpoint: "",
+      redirectUrl: "",
+      proxyUrl: "",
+    },
+    imgur: {
+      clientId: "",
+      clientSecret: "",
+    },
+  },
+  daysuntil: {},
+  gamedayposts: {
+    channelId: "",
+    tagIds: {
+      preseason: "",
+      regularSeason: "",
+      postSeason: "",
+      seasons: [],
+    },
+  },
+  healthcheck: {},
+  imageproxy: {},
+  joinleave: {
+    channelId: "",
+  },
+  levels: {},
+  misc: {},
+  nhl: {},
+  playeremojis: {},
+  reactionrole: {
+    channelId: "",
+    reactionRoles: [],
+  },
+  registercommands: {},
+  statsvoicechannel: {
+    channels: [],
+  },
+  usermanagement: {
+    channelId: "",
+  },
+  vistorrole: {
+    memberRoleId: "",
+    visitorRoleId: "",
+    visitorEmojiId: "",
+    rolesChannelId: "",
+  },
+};

df9f6f9f188f8409603958ed52ad2f0e2d64ecd8
default <mstrenk12@gmail.com>
2025-06-29 19:55:07 -0400

    Fix path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 echo "Backing up current DB..."
-sudo /home/matt/dockers/flyerscord/scripts/backupDBFile.sh flyerscord-discord-prod-bot-1 /home/matt/backups/flyerscord-discord-prod-bot
+sudo /home/matt/dockers/flyerscord/scripts/cron/backupDBFile.sh flyerscord-discord-prod-bot-1 /home/matt/backups/flyerscord-discord-prod-bot
 echo "Back up complete!"
 
 echo "Building new image and deploying..."

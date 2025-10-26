387ee5eb86a91c9b0fdeca48bb2b8af62b0e54ee
default <mstrenk12@gmail.com>
2025-10-26 04:21:45 -0400

    Add basic normalize class for rules module. Still need to fill it in

--- src/modules/rules/migration/normalize.ts
+++ src/modules/rules/migration/normalize.ts
@@ -1,1 +1,18 @@
+import Normalize from "@root/src/common/migration/Normalize";
+
 // TODO: Add rules normalization ( Going to be a major overhaul )
+export default class RulesNormalize extends Normalize {
+  constructor() {
+    super("Rules");
+  }
+
+  async normalize(): Promise<void> {}
+
+  protected async runValidation(): Promise<boolean> {
+    return false;
+  }
+
+  private async migrateRules(): Promise<number> {
+    return 0;
+  }
+}

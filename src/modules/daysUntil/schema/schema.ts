f330cd1b438f4878c8886224b49ee93d2c4005c2
default <mstrenk12@gmail.com>
2025-10-25 22:47:32 -0400

    Create migration for dayuntils

---
+++ src/modules/daysUntil/schema/schema.ts
@@ -0,0 +1,13 @@
+import { createModuleTable } from "@root/src/common/db/schema-types";
+import { boolean, serial, timestamp, varchar } from "drizzle-orm/pg-core";
+
+export const daysUntilDates = createModuleTable("days_until__dates", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 255 }).notNull().unique(),
+  date: timestamp("date"),
+  enabled: boolean("enabled").notNull().default(false),
+});
+
+export default {
+  daysUntilDates,
+};

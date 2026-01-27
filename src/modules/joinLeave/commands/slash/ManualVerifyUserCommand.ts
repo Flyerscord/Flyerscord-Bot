811b043efc3beb1513e831b784a27740e380074d
default <mstrenk12@gmail.com>
2026-01-27 05:11:46 +0000

    Add command for manually verifying users

---
+++ src/modules/joinLeave/commands/slash/ManualVerifyUserCommand.ts
@@ -0,0 +1,39 @@
+import { AdminSlashCommand, PARAM_TYPES } from "@common/models/SlashCommand";
+import ConfigManager from "@common/managers/ConfigManager";
+import discord from "@common/utils/discord/discord";
+import { ChatInputCommandInteraction, User } from "discord.js";
+import JoinLeaveDB from "../../db/JoinLeaveDB";
+
+export default class ManualVerifyUserCommand extends AdminSlashCommand {
+  constructor() {
+    super("verifyuser", "Manually mark a user as verified", { ephermal: true });
+
+    this.data.addUserOption((option) => option.setName("user").setDescription("The user manually verify").setRequired(true));
+  }
+
+  async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+    const user = this.getParamValue(interaction, PARAM_TYPES.USER, "user") as User;
+
+    const member = await discord.members.getMember(user.id, true);
+    if (!member) {
+      await this.replies.reply(`User ${user.id} is not in the server!`);
+      return;
+    }
+
+    const db = new JoinLeaveDB();
+
+    const notVerifiedUser = await db.getNotVerifiedUser(user.id);
+    if (!notVerifiedUser) {
+      await this.replies.reply(`User ${user.id} is already verified!`);
+      return;
+    }
+
+    const notVerifiedRoleId = ConfigManager.getInstance().getConfig("JoinLeave").notVerifiedRoleId;
+    await discord.roles.removeRoleFromUser(member, notVerifiedRoleId);
+    await db.deleteNotVerifiedUser(user.id);
+
+    await discord.messages.sendMesssageDMToUser(user.id, "You have been verified! You can now take part in the Go Flyers Server!");
+
+    await this.replies.reply(`User ${user.id} has been verified!`);
+  }
+}

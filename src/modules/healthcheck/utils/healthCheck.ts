8d9b70cbac66f71be9558e74e2c6458be5b5c876
default <mstrenk12@gmail.com>
2026-01-31 18:15:57 +0000

    Remove the tertiary version instead switch to using the commit number for the tertiary version

--- src/modules/healthcheck/utils/healthCheck.ts
+++ src/modules/healthcheck/utils/healthCheck.ts
@@ -7,12 +7,14 @@ import { version } from "@root/package.json";
 export function getBotHealth(): IBotHealth {
   const client = ClientManager.getInstance().client;
 
+  const fullVersion = getFullVersion();
+
   const healthManager = BotHealthManager.getInstance();
   if (client.isReady() && healthManager.isHealthy()) {
     return {
       status: "healthy",
       message: "Bot is connected to Discord",
-      version: version,
+      version: fullVersion,
       uptime: process.uptime(),
       botUptime: client.uptime,
     };
@@ -20,7 +22,12 @@ export function getBotHealth(): IBotHealth {
     return {
       status: "unhealthy",
       message: "Bot is not connected to Discord or did not start up correctly",
-      version: version,
+      version: fullVersion,
     };
   }
+}
+
+function getFullVersion(): string {
+  const commitCount = process.env.COMMIT_COUNT || "dev";
+  return `${version}.${commitCount}`;
 }
